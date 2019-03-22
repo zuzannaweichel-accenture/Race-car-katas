ceedf53517bb4210e0b0f4566aca98ead92d0d55
zuzannaweichel-accenture <zuzanna.weichel@accenture.com>
2019-03-22 16:33:13 -0400

    refactored project test for check transmission added

--- Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -21,4 +21,17 @@ public class TelemetryDiagnosticControlsTest {
         assertEquals(message, diagnosticControls.checkTransmission());
 	}
 
+	@Test
+    public void checkTransmission_retryConnection3Times_ifOnlineStatusFalse(){
+        TelemetryClient telemetryClientMock = mock(TelemetryClient.class);
+        when(telemetryClientMock.getOnlineStatus()).thenReturn(false).thenReturn(false).thenReturn(false).thenReturn(false);
+        TelemetryDiagnosticControls diagnosticControls = new TelemetryDiagnosticControls(telemetryClientMock);
+        String eMessage = "";
+        try{
+           String diagnosticsInfo =  diagnosticControls.checkTransmission();
+        }catch (Exception e){
+            eMessage = e.getMessage();
+        }
+        assertEquals("Unable to connect.", eMessage);
+    }
 }
